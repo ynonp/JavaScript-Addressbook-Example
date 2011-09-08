81ad5ab9d149ea116c1c8ba66e72847e8cde0868
ynonp <ynonperek@gmail.com>
2011-09-08 17:01:54 +0300

    In memory storage

---
+++ main.js
@@ -0,0 +1,9 @@
+(function(global) {
+
+    var store = global.store;
+    store.addContact(store.ContactInfo('Bill', '04-9112212', 'bill@gmail.com'));
+    store.addContact(store.ContactInfo('James', '02-9331232', 'james@yahoo.com'));
+    store.addContact(store.ContactInfo('Judy', '03-9331313', 'judy@hotmail.com'));
+
+}(this));
+
---
+++ store.js
@@ -0,0 +1,31 @@
+(function(global) {
+
+    var store = { contacts : [] };
+
+    store.ContactInfo = function(name, phone, email) {
+        return { name : name, phone : phone, email : email }
+    };
+
+    store.addContact = function(contact) {
+        console.log('add contact: ');
+        console.dir(contact);
+        store.contacts.push(contact);
+        console.log('ok');
+    };
+
+    store.removeContact = function(contact) {
+        for (var i=0; i < store.contacts.length; ++i ) {
+            if ( store.contacts[i].phone === contact.phone ) {
+                // found it
+                store.contacts.splice(i);
+                break;
+            }
+        }
+    };
+
+    store.getContacts = function() {
+        return store.contacts;
+    };
+
+    global.store = store;
+}(this));
